e9d28d493a78353b4ad3ebc39cef366297a5e5f4
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2020-08-13 17:34:22 +0900

    third

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,94 @@
 <template>
   <div class="home">
-    <div class="search-box">
-      <form action="">
-        <input type="url" class="search" placeholder="URL検索">
-        <button type="submit" class="submit-button">
-          <font-awesome-icon icon="search" class="search-icon"/>
-        </button>
-      </form>
-    </div>
+      <div class="search-box">
+        <form action="">
+          <input type="url" class="search" placeholder="URL検索">
+          <button type="submit" class="submit-button">
+            <font-awesome-icon icon="search" class="search-icon"/>
+          </button>
+        </form>
+      </div>
 
-    <div class="youtube-box">
-      <div class="video-box">
-        <div class="video">
+      <div class="youtube-box">
+        <div class="video-box">
+          <div class="video">
 
-        </div>
+          </div>
         <h1>タイトル</h1>
       </div>
 
-      <div id="coment" class="coment-box">
-        <div class="coment">
-          <Coment />
-        </div>
-        <div class="stamp-box">
-          <button class="stamp">スタンプ購入</button>
+        <div id="coment" class="coment-box">
+          <div class="coment">
+
+          </div>
+          <div class="stamp-box">
+            <button class="stamp">スタンプ購入</button>
+          </div>
         </div>
       </div>
-    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 //import StampButton from "@/components/StampButton.vue";
-import Coment from "../components/coment.vue";
+//import Coment from "../components/coment.vue";
 
 export default {
   name: "Home",
-  components: {
-    Coment
-  }
 };
 </script>
+
+<style scoped>
+  body{
+    background-color: #000;
+  }
+  .search-box{
+    padding-top: 50px;
+    text-align: center;
+  }
+  .search{
+    width: 40em;
+    height: 30px;
+  }
+  .submit-button{
+    width: 50px;
+    height: 36px;
+    cursor: pointer;
+  }
+  .search-icon{
+    color: rgb(116, 116, 116);
+    font-size: 10px;
+  }
+  .youtube-box{
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+    padding-top: 50px;
+  }
+  .video-box{
+    width: 60%;
+  }
+  .video{
+    border: #fff solid 2px;
+    width: 100%;
+    height: 56.25vh;
+  }
+  .coment-box{
+    width: 20%;
+    margin-left: 20px;
+  }
+  .coment{
+    border: #fff solid 2px;
+    width: 100%;
+    height: 56.25vh;
+  }
+  .stamp{
+    background-color: rgb(142, 248, 2);
+    color: rgb(17, 0, 252);
+    cursor: pointer;
+    outline: none;
+    padding: 10px 30px;
+    margin-top: 10px;
+  }
+</style>
