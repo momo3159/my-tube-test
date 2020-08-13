727b44af28464d2221a7d8b61f237ae3d5b40a1d
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2020-08-13 17:17:03 +0900

    hoge

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/MyTubeTest/' : '/',
+      ? '/my-tube-test/' : '/',
     assetsDir: "./",
     outputDir: "docs",
   };
